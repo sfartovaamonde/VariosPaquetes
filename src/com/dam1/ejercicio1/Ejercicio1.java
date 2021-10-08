147d168760d8026a560380c2710bb785710cff76
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-10-08 09:34:06 +0200

    Inicio proyecto

---
+++ src/com/dam1/ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+package com.dam1.ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("Ejercicio1");
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/dam1/ejercicio2/Ejercicio2.java
@@ -0,0 +1,8 @@
+package com.dam1.ejercicio2;
+public class Ejercicio2 {
+    public static void otra/String[]args) {
+    System.out.println("nueva");
+
+}
+
+}
